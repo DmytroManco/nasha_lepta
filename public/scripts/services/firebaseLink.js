a46ea98ecb23409ff05adb0560b614ad187eec65
Manco <moncodp@gmail.com>
2016-03-13 14:19:22 +0200

    firebase-link #7

--- public/scripts/services/firebaseLink.js
+++ public/scripts/services/firebaseLink.js
@@ -8,4 +8,4 @@
  * Constant in the nashaLeptaApp.
  */
 angular.module('nashaLeptaApp')
-  .constant('FirebaseLink', 'https://boiling-fire-9518.firebaseio.com');
+  .constant('FirebaseLink', 'https://blinding-fire-1074.firebaseio.com');
